3c1755cdb0f96c88e10c9ce6effc3e720576d7af
1053818949@qq.com <1053818949@qq.com>
2018-09-28 10:33:23 +0800

    2018年9月28日10:27:51   test

---
+++ src/main/java/com/cdzic/spring_test/web/TestController.java
@@ -0,0 +1,17 @@
+package com.cdzic.spring_test.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @creator yaotao
+ * @date 2018/9/28 10:17
+ * @describe:
+ */
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String test1(){
+        return "帅哥你好！";
+    }
+}
